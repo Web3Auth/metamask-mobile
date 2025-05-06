cc8b77e52f18f3df47c9a41fb0b1590d069db018
ieow <cwlee0@gmail.com>
2025-05-06 17:32:39 +0800

    feat: setting reveal multi-srp screen

--- app/components/Views/ProtectYourWallet/index.tsx
+++ app/components/Views/ProtectYourWallet/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { useTheme } from '../../../util/theme';
 import Text, {
@@ -8,23 +8,34 @@ import Text, {
 import { strings } from '../../../../locales/i18n';
 import { getNavigationOptionsTitle } from '../../UI/Navbar';
 import { useNavigation } from '@react-navigation/native';
-import Icon, {
-  IconName,
-  IconSize,
-  IconColor,
-} from '../../../component-library/components/Icons/Icon';
-import AccountDetailsBox from './AccountDetailsBox';
+import SelectSRP from '../SelectSRP';
+import { useSelector } from 'react-redux';
+import {
+  selectSeedlessOnboardingAuthConnection,
+  selectSeedlessOnboardingUserEmail,
+  selectSeedlessOnboardingUserId,
+} from '../../../selectors/seedlessOnboardingController';
+import Banner, {
+  BannerAlertSeverity,
+  BannerVariant,
+} from '../../../component-library/components/Banners/Banner';
 
 const ProtectYourWallet = () => {
   const { colors } = useTheme();
   const navigation = useNavigation();
+  const authConnection = useSelector(selectSeedlessOnboardingAuthConnection);
+  const userEmail = useSelector(selectSeedlessOnboardingUserEmail);
+  const seedlessOnboardingUserId = useSelector(selectSeedlessOnboardingUserId);
+
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const styles = StyleSheet.create({
     root: {
       flex: 1,
-      padding: 16,
       flexDirection: 'column',
-      rowGap: 16,
+    },
+    socialBox: {
+      padding: 16,
+      marginHorizontal: 8,
     },
     box: {
       backgroundColor: colors.background.muted,
@@ -33,12 +44,15 @@ const ProtectYourWallet = () => {
       flexDirection: 'row',
       alignItems: 'center',
       justifyContent: 'space-between',
-      columnGap: 8,
     },
     boxRight: {
       flexDirection: 'row',
       alignItems: 'center',
       columnGap: 8,
+    },
+    accessory: {
+      marginTop: 16,
+      marginHorizontal: 24,
     },
   });
 
@@ -53,37 +67,42 @@ const ProtectYourWallet = () => {
     );
   }, [navigation, colors]);
 
+  const [finalUserEmail, setFinalUserEmail] = useState(userEmail);
+  useEffect(() => {
+    if (userEmail) {
+      if (userEmail.endsWith('@privaterelay.appleid.com')) {
+        setFinalUserEmail(undefined);
+      } else {
+        setFinalUserEmail(userEmail);
+      }
+    }
+  }, [userEmail]);
+
   return (
     <View style={styles.root}>
-      <View style={styles.box}>
-        <Text variant={TextVariant.BodyMDMedium} color={TextColor.Default}>
-          {strings('protect_your_wallet.login_with_social')}
-        </Text>
-        <View style={styles.boxRight}>
-          <Text variant={TextVariant.BodyMDMedium} color={TextColor.Error}>
-            {strings('protect_your_wallet.setup')}
-          </Text>
-          <Icon
-            name={IconName.ArrowRight}
-            size={IconSize.Lg}
-            color={IconColor.Alternative}
-          />
-        </View>
-      </View>
+      {authConnection && seedlessOnboardingUserId && (
+        <Banner
+          variant={BannerVariant.Alert}
+          severity={BannerAlertSeverity.Success}
+          title={strings('protect_your_wallet.login_with_social')}
+          description={
+            <Text variant={TextVariant.BodyMD} color={TextColor.Default}>
+              {finalUserEmail}
+            </Text>
+          }
+          style={styles.accessory}
+        />
+      )}
+      {!authConnection && !seedlessOnboardingUserId && (
+        <Banner
+          variant={BannerVariant.Alert}
+          severity={BannerAlertSeverity.Error}
+          title={strings('app_settings.social_login_linked')}
+          style={styles.accessory}
+        />
+      )}
 
-      <AccountDetailsBox
-        type="backup"
-        title={strings('protect_your_wallet.secret_recovery_phrase', {
-          num: 1,
-        })}
-      />
-
-      <AccountDetailsBox
-        type="reveal"
-        title={strings('protect_your_wallet.secret_recovery_phrase', {
-          num: 2,
-        })}
-      />
+      <SelectSRP />
     </View>
   );
 };
--- app/selectors/seedlessOnboardingController.ts
+++ app/selectors/seedlessOnboardingController.ts
@@ -2,18 +2,23 @@ import { createSelector } from 'reselect';
 import { RootState } from '../reducers';
 import { SeedlessOnboardingControllerState } from '@metamask/seedless-onboarding-controller';
 
-
 const selectSeedlessOnboardingControllerState = (state: RootState) =>
-    state.engine?.backgroundState.SeedlessOnboardingController;
+  state.engine?.backgroundState.SeedlessOnboardingController;
 
 export const selectSeedlessOnboardingUserId = createSelector(
-    selectSeedlessOnboardingControllerState,
-    (seedlessOnboardingControllerState: SeedlessOnboardingControllerState) =>
-      seedlessOnboardingControllerState.userId,
+  selectSeedlessOnboardingControllerState,
+  (seedlessOnboardingControllerState: SeedlessOnboardingControllerState) =>
+    seedlessOnboardingControllerState.userId,
 );
 
 export const selectSeedlessOnboardingUserEmail = createSelector(
-    selectSeedlessOnboardingControllerState,
-    (seedlessOnboardingControllerState: SeedlessOnboardingControllerState) =>
-      seedlessOnboardingControllerState.socialLoginEmail,
+  selectSeedlessOnboardingControllerState,
+  (seedlessOnboardingControllerState: SeedlessOnboardingControllerState) =>
+    seedlessOnboardingControllerState.socialLoginEmail,
+);
+
+export const selectSeedlessOnboardingAuthConnection = createSelector(
+  selectSeedlessOnboardingControllerState,
+  (seedlessOnboardingControllerState: SeedlessOnboardingControllerState) =>
+    seedlessOnboardingControllerState.authConnection,
 );
