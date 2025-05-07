65d6ad48f9b0befa20966aad9a047fa39a1fd62f
ieow <cwlee0@gmail.com>
2025-05-07 15:42:06 +0800

    fix: shim to fix broken solana account creation

--- app/core/Engine/controllers/seedless-onboarding-controller/shim.js
+++ app/core/Engine/controllers/seedless-onboarding-controller/shim.js
@@ -1,3 +1,12 @@
-import Crypto from 'react-native-quick-crypto';
+// import Crypto from 'react-native-quick-crypto';
+// eslint-disable-next-line import/no-nodejs-modules
+// import Crypto from 'react-native-crypto';
+// eslint-disable-next-line import/no-nodejs-modules
+import Crypto from 'crypto';
 
-global.crypto = Crypto;
+// TODO: FIX ME
+// eslint-disable-next-line no-console
+global.crypto = {
+  ...Crypto,
+  ...global.crypto,
+};
