f4b5bdb28a71353886ffef2059ea7ebde9203464
huggingbot <83656073+huggingbot@users.noreply.github.com>
2025-06-01 11:27:58 +0800

    feat: Update learn more link tracking in ChoosePassword component

--- app/components/Views/ChoosePassword/index.js
+++ app/components/Views/ChoosePassword/index.js
@@ -610,10 +610,16 @@ class ChoosePassword extends PureComponent {
   };
 
   learnMore = () => {
+    const learnMoreUrl = 'https://support.metamask.io/managing-my-wallet/resetting-deleting-and-restoring/how-can-i-reset-my-password/';
+    this.track(MetaMetricsEvents.EXTERNAL_LINK_CLICKED, {
+      text: 'Learn More',
+      location: 'choose_password',
+      url: learnMoreUrl,
+    });
     this.props.navigation.push('Webview', {
       screen: 'SimpleWebview',
       params: {
-        url: 'https://support.metamask.io/managing-my-wallet/resetting-deleting-and-restoring/how-can-i-reset-my-password/',
+        url: learnMoreUrl,
         title: 'support.metamask.io',
       },
     });
